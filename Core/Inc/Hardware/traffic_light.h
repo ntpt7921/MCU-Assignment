abb4274d9a9d4737f4487cb3b67954e1acb5eaf7
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-12-29 19:35:53 +0700

    Add hardware module for traffic light

---
+++ Core/Inc/Hardware/traffic_light.h
@@ -0,0 +1,33 @@
+/*
+ * traffic_light.h
+ *
+ *  Created on: Dec 29, 2022
+ *      Author: ntpt
+ */
+
+#ifndef INC_HARDWARE_TRAFFIC_LIGHT_H_
+#define INC_HARDWARE_TRAFFIC_LIGHT_H_
+
+#include "main.h"
+#include "stm32f103xb.h"
+
+typedef enum
+{
+    COLOR_RED = 0,
+    COLOR_YELLOW,
+    COLOR_GREEN,
+    COLOR_NONE,
+} TrafficLightColor_t;
+
+typedef struct
+{
+    TrafficLightColor_t current_color;
+    GPIO_TypeDef *port;
+    uint16_t pin[2];
+
+} TrafficLight_t;
+
+void Hardware_TrafficLight_SetColor(TrafficLight_t *tf, TrafficLightColor_t color);
+void Hardware_TrafficLight_TurnOff(TrafficLight_t *tf);
+
+#endif /* INC_HARDWARE_TRAFFIC_LIGHT_H_ */
---
+++ Core/Src/Hardware/traffic_light.c
@@ -0,0 +1,44 @@
+/*
+ * traffic_light.c
+ *
+ *  Created on: Dec 29, 2022
+ *      Author: ntpt
+ */
+
+#include "Hardware/traffic_light.h"
+#include "stm32f1xx_hal_gpio.h"
+
+void Hardware_TrafficLight_SetColor(TrafficLight_t *tf, TrafficLightColor_t color)
+{
+    switch (color)
+    {
+        case COLOR_NONE:
+            // 00
+            HAL_GPIO_WritePin(tf->port, tf->pin[0], GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(tf->port, tf->pin[1], GPIO_PIN_RESET);
+            break;
+        case COLOR_RED:
+            // 10
+            HAL_GPIO_WritePin(tf->port, tf->pin[0], GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(tf->port, tf->pin[1], GPIO_PIN_SET);
+            break;
+        case COLOR_YELLOW:
+            // 11
+            HAL_GPIO_WritePin(tf->port, tf->pin[0], GPIO_PIN_SET);
+            HAL_GPIO_WritePin(tf->port, tf->pin[1], GPIO_PIN_SET);
+            break;
+        case COLOR_GREEN:
+            // 01
+            HAL_GPIO_WritePin(tf->port, tf->pin[0], GPIO_PIN_SET);
+            HAL_GPIO_WritePin(tf->port, tf->pin[1], GPIO_PIN_RESET);
+            break;
+        default:
+            // do nothing
+            break;
+    }
+}
+void Hardware_TrafficLight_TurnOff(TrafficLight_t *tf)
+{
+    Hardware_TrafficLight_SetColor(tf, COLOR_NONE);
+}
+
